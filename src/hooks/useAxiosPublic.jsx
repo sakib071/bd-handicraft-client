7dae7d285a48c1935fa0409a6dc3b02ed1d6c646
sakib071 <c193055@ugrad.iiuc.ac.bd>
2024-08-25 13:32:41 +0600

    minor updates

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,14 +1,3 @@
-// import axios from 'axios';
-
-// const axiosPublic = axios.create({
-//     baseURL: 'http://localhost:5000'
-// });
-// const useAxiosPublic = () => {
-//     return axiosPublic;
-// };
-
-// export default useAxiosPublic;
-
 import axios from 'axios';
 
 const useAxiosPublic = () => {
